d1df2b3f6f100e1a0fa6e47cf70f7bc3a78e9535
Jeremy Haile <jhaile@gmail.com>
2018-05-30 22:16:11 -0400

    Add API endpoints for pdf mage.

--- lib/pdf_mage.rb
+++ lib/pdf_mage.rb
@@ -2,6 +2,10 @@ require 'ostruct'
 require 'yaml'
 require 'pdf_mage/version'
 require 'pdf_mage/api/app'
+require 'pdf_mage/workers/base'
+require 'pdf_mage/workers/render_pdf'
+require 'pdf_mage/workers/send_webhook'
+require 'pdf_mage/workers/upload_file'
 
 config_file = File.new(File.expand_path('lib/pdf_mage/config.yml'))
 $config = OpenStruct.new(YAML.load(config_file)).freeze
--- lib/pdf_mage/api/app.rb
+++ lib/pdf_mage/api/app.rb
@@ -1,10 +1,42 @@
 require 'sinatra'
+require 'json'
 
 module PdfMage
   module Api
     class App < Sinatra::Base
-      get '/' do
-        'Hello world!'
+      set :environment, :production
+
+      before do
+        content_type :json
+        error(401, 'Unauthorized') unless params[:token].strip == $config.api_secret
+      end
+
+      error do
+        e = env['sinatra.error']
+        error(500, e.message)
+      end
+
+      get '/api/ping' do
+        'pong'
+      end
+
+      post '/api/render' do
+        url          = required_param(:url)
+        callback_url = required_param(:callback_url)
+        meta         = params[:meta]
+
+        job_id = PdfMage::Workers::RenderPdf.perform_async(url, callback_url, meta)
+        {result: 'ok', job_id: job_id}.to_json
+      end
+
+      def error(code, message)
+        halt code, {result: 'error', message: message}.to_json
+      end
+
+      def required_param(key)
+        value = params[key]
+        error(422, "Required parameter '#{key}' is missing") unless value && value.length > 0
+        value
       end
     end
   end
