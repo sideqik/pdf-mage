37543ab0b353b047cce520996ec1ff100c4abf0f
Ben Hirsch <benhirsch42@gmail.com>
2019-08-27 04:33:20 +0100

    Try to improve waiting for network requests

--- print-to-pdf/index.js
+++ print-to-pdf/index.js
@@ -5,11 +5,56 @@ let url = argv.url;
 let path = argv.path;
 let format = argv.format || 'Letter';
 
+// https://github.com/GoogleChrome/puppeteer/issues/1353#issuecomment-356561654
+function waitForNetworkIdle(page, timeout, maxInflightRequests = 0) {
+  page.on('request', onRequestStarted);
+  page.on('requestfinished', onRequestFinished);
+  page.on('requestfailed', onRequestFinished);
+
+  let inflight = 0;
+  let fulfill;
+  let promise = new Promise(x => fulfill = x);
+  let timeoutId = setTimeout(onTimeoutDone, timeout);
+  return promise;
+
+  function onTimeoutDone() {
+    page.removeListener('request', onRequestStarted);
+    page.removeListener('requestfinished', onRequestFinished);
+    page.removeListener('requestfailed', onRequestFinished);
+    fulfill();
+  }
+
+  function onRequestStarted() {
+    ++inflight;
+    if (inflight > maxInflightRequests)
+      clearTimeout(timeoutId);
+  }
+
+  function onRequestFinished() {
+    if (inflight === 0)
+      return;
+    --inflight;
+    if (inflight === maxInflightRequests)
+      timeoutId = setTimeout(onTimeoutDone, timeout);
+  }
+}
+
+function delay(timeout) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, timeout);
+  });
+}
+
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.setViewport({ width: 1250, height: 1650 }); // by trial-and-error, this makes charts render at the right width
-  await page.goto(url, { waitUntil: 'networkidle0' });
+
+  await Promise.all([
+    waitForNetworkIdle(page, 3000),
+    page.goto(url),
+  ]);
+
   await page.pdf({ path, format, printBackground: true });
   await browser.close();
 })();
