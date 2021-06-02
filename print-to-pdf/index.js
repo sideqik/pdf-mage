de66251053c9e9d7b150ef3e904ee60480bfb402
Michael Farrell <michaelmerrittfarrell@gmail.com>
2021-06-02 13:29:12 -0400

    Modified to display in landscape by default. Gave ability to override height and width via args

--- print-to-pdf/index.js
+++ print-to-pdf/index.js
@@ -4,8 +4,16 @@ const Promise = require('bluebird');
 
 let url = argv.url;
 let path = argv.path;
+
+// Landscape / Portrait
+let isPixels = true;
+let isLandscape = true;
+let longerLengthValue = isPixels ? 1270 : '11in';
+let shorterLengthValue = isPixels ? 720 : '8.5in';
+let width = argv.width || isLandscape ? longerLengthValue : shorterLengthValue;
+let height = argv.height || isLandscape ? shorterLengthValue : longerLengthValue;
+
 let safetySeconds = argv.delay;
-let format = argv.format || 'Letter';
 let scale = parseFloat(argv.scale) || 1;
 
 const TIME_SINCE_LAST_REQUEST = 3000;
@@ -52,23 +60,31 @@ function delay(timeout) {
 }
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.setViewport({ width: 1250, height: 1650 }); // by trial-and-error, this makes charts render at the right width
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.setViewport({ width, height });
 
-  await Promise.all([
-    // IF a) all requests have resolved an no new requests have been made in 3 seconds, OR
-    //    b) 15 seconds have elapsed
-    // THEN move on to rendering to PDF
-    Promise.any([
-      new Promise(resolve => setTimeout(resolve, MAX_RENDER_WAIT)),
-      waitForNetworkIdle(page, TIME_SINCE_LAST_REQUEST),
-    ]),
-    page.goto(url),
-  ]).catch(() => process.exit(1));
+	await Promise.all([
+		// IF a) all requests have resolved an no new requests have been made in 3 seconds, OR
+		//    b) 15 seconds have elapsed
+		// THEN move on to rendering to PDF
+		Promise.any([
+			new Promise((resolve) => setTimeout(resolve, MAX_RENDER_WAIT)),
+			waitForNetworkIdle(page, TIME_SINCE_LAST_REQUEST),
+		]),
+		page.goto(url),
+	]).catch(() => process.exit(1));
 
-  await delay(safetySeconds * 1000);
+	await delay(safetySeconds * 1000);
 
-  await page.pdf({ path, format, scale, printBackground: true }).catch(() => process.exit(1));
-  await browser.close();
+	await page
+    .pdf({
+      path,
+      width: width + 'px',
+      height: height + 'px',
+      scale,
+      printBackground: true
+    })
+		.catch(() => process.exit(1));
+	await browser.close();
 })();
