da5145b331aa6bee7aaf9cdf92cc104faa7d0f67
Michael Farrell <michaelmerrittfarrell@gmail.com>
2021-06-24 11:02:41 -0400

    Added dev script and reimplemented landscape code

--- print-to-pdf/index.js
+++ print-to-pdf/index.js
@@ -4,8 +4,16 @@ const Promise = require('bluebird');
 
 let url = argv.url;
 let path = argv.path;
+
+// Landscape / Portrait
+let longerDimension = 1270;
+let shorterDimension = 720;
+
+let isLandscape = argv.landscape || true;
+let width = argv.width || isLandscape ? longerDimension : shorterDimension;
+let height = argv.height || isLandscape ? shorterDimension : longerDimension;
+
 let safetySeconds = argv.delay;
-let format = argv.format || 'Letter';
 let scale = parseFloat(argv.scale) || 1;
 
 const TIME_SINCE_LAST_REQUEST = 3000;
@@ -13,63 +21,68 @@ const MAX_RENDER_WAIT = 15000;
 
 // https://github.com/GoogleChrome/puppeteer/issues/1353#issuecomment-356561654
 function waitForNetworkIdle(page, timeout, maxInflightRequests = 0) {
-  page.on('request', onRequestStarted);
-  page.on('requestfinished', onRequestFinished);
-  page.on('requestfailed', onRequestFinished);
+	page.on('request', onRequestStarted);
+	page.on('requestfinished', onRequestFinished);
+	page.on('requestfailed', onRequestFinished);
 
-  let inflight = 0;
-  let fulfill;
-  let promise = new Promise(x => fulfill = x);
-  let timeoutId = setTimeout(onTimeoutDone, timeout);
-  return promise;
+	let inflight = 0;
+	let fulfill;
+	let promise = new Promise((x) => (fulfill = x));
+	let timeoutId = setTimeout(onTimeoutDone, timeout);
+	return promise;
 
-  function onTimeoutDone() {
-    page.removeListener('request', onRequestStarted);
-    page.removeListener('requestfinished', onRequestFinished);
-    page.removeListener('requestfailed', onRequestFinished);
-    fulfill();
-  }
+	function onTimeoutDone() {
+		page.removeListener('request', onRequestStarted);
+		page.removeListener('requestfinished', onRequestFinished);
+		page.removeListener('requestfailed', onRequestFinished);
+		fulfill();
+	}
 
-  function onRequestStarted() {
-    ++inflight;
-    if (inflight > maxInflightRequests)
-      clearTimeout(timeoutId);
-  }
+	function onRequestStarted() {
+		++inflight;
+		if (inflight > maxInflightRequests) clearTimeout(timeoutId);
+	}
 
-  function onRequestFinished() {
-    if (inflight === 0)
-      return;
-    --inflight;
-    if (inflight === maxInflightRequests)
-      timeoutId = setTimeout(onTimeoutDone, timeout);
-  }
+	function onRequestFinished() {
+		if (inflight === 0) return;
+		--inflight;
+		if (inflight === maxInflightRequests)
+			timeoutId = setTimeout(onTimeoutDone, timeout);
+	}
 }
 
 function delay(timeout) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, timeout);
-  });
+	return new Promise((resolve) => {
+		setTimeout(resolve, timeout);
+	});
 }
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.setViewport({ width: 1250, height: 1650 }); // by trial-and-error, this makes charts render at the right width
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.setViewport({ width: width - 1 , height: height - 1 });
 
-  await Promise.all([
-    // IF a) all requests have resolved an no new requests have been made in 3 seconds, OR
-    //    b) 15 seconds have elapsed
-    // THEN move on to rendering to PDF
-    Promise.any([
-      new Promise(resolve => setTimeout(resolve, MAX_RENDER_WAIT)),
-      waitForNetworkIdle(page, TIME_SINCE_LAST_REQUEST),
-    ]),
-    page.goto(url),
-  ]).catch(() => process.exit(1));
+	await Promise.all([
+		// IF a) all requests have resolved an no new requests have been made in 3 seconds, OR
+		//    b) 15 seconds have elapsed
+		// THEN move on to rendering to PDF
+		Promise.any([
+			new Promise((resolve) => setTimeout(resolve, MAX_RENDER_WAIT)),
+			waitForNetworkIdle(page, TIME_SINCE_LAST_REQUEST),
+		]),
+		page.goto(url),
+	]).catch(() => process.exit(1));
 
-  await delay(safetySeconds * 1000);
+	await delay(safetySeconds * 1000);
 
-  await page.pdf({ path, format, scale, printBackground: true }).catch(() => process.exit(1));
-  await browser.close();
+	await page
+		.pdf({
+			path,
+			width: width + 'px',
+			height: height + 'px',
+			scale,
+			printBackground: true,
+		})
+		.catch(() => process.exit(1));
+	await browser.close();
 })();
-
